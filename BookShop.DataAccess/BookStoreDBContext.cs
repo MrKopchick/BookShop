6325f0edd0f1ef3c361e8c95d01389ca711e21e4
Illya <illakoroluk62@gmail.com>
2025-03-11 23:44:09 +0200

    Добавьте файлы проекта.

---
+++ BookShop.DataAccess/BookStoreDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.DataAccess
+{
+    public class BookStoreDBContext : DbContext
+    {
+        public BookStoreDBContext(DbContextOptions<BookStoreDBContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
