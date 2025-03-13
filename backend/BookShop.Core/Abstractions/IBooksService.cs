10fbcf1650b34e93728f382712067799ecbea421
Illya <illakoroluk62@gmail.com>
2025-03-13 19:10:53 +0200

    add bll services

---
+++ backend/BookShop.Core/Abstractions/IBooksService.cs
@@ -0,0 +1,12 @@
+﻿using BookShop.Core.Models;
+
+namespace BookShop.Core.Abstractions
+{
+    public interface IBooksService
+    {
+        Task<Guid> CreateBookAsync(Book book);
+        Task<Guid> DeleteBookAsync(Guid id);
+        Task<List<Book>> GetAllBooksAsync();    
+        Task<Guid> UpdateBookAsync(Guid id, string title, string description, decimal price);
+    }
+}
